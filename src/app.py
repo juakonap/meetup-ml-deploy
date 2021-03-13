03564eaa1501cd522d8700af0c02eed701fe9c70
juakonap <juakonap@gmail.com>
2021-03-13 18:29:53 -0300

    ADD: heroku adapt

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+server = app.server
 
 from flask_cors import CORS
 CORS(app)
@@ -13,7 +14,7 @@ def home():
     return '<h1> La aplicación está funcionando!</h1>'
 
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
     import pickle
     lgbm = pickle.load(open('hr_ds-lgbm-model.ml', 'rb'))
@@ -32,7 +33,9 @@ def predict():
                            ]])
     return str(round(target[0],3))
 
+'''
 if __name__ == "__main__":
     app.run(debug=True)
+'''
 
 #http://127.0.0.1:5000/predict?city=5&city_development_index=0.92&gender=1&relevent_experience=0&enrolled_university=3&education_level=3&major_discipline=1&experience=21&company_size=8&company_type=3&last_new_job=0&training_hours=36
